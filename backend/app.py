25c0bf49dae46134fe95eebd3e63b3490110b0cd
jessicatracyliu <jliu3080@gatech.edu>
2024-03-09 10:40:55 -0500

    updated logic to test LED

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,17 @@ GPIO.setup(18, GPIO.OUT)
 def index():
     return 'Hello world'
 
+@app.route('/',methods=['GET'])
+def test():
+    status = request.args.get('status')
+    if status == "on":
+        GPIO.output(18, GPIO.HIGH)
+        return "light on"
+    elif status == "off"
+        GPIO.output(18, GPIO.LOW)
+        return "light off"
+    else:
+        return "uh oh"
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-    GPIO.output(18, GPIO.HIGH)
