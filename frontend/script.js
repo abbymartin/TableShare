e3410787a86f10ee59872b90d6fd84bb8fd23509
Christine <132790780+aesteri@users.noreply.github.com>
2024-03-09 11:26:14 -0500

    Fixed popup button

--- frontend/script.js
+++ frontend/script.js
@@ -2,22 +2,26 @@ function handleSelection() {
     var selectElement = document.getElementById("occupancy");
     var selectedValue = selectElement.value;
     var table = document.getElementById("rectangle");
-    var popup = document.getElementById("popup");
-    popup.style.visibility = "visible";
+    
     
     // Perform actions based on the selected option
     switch(selectedValue) {
         case "consolidate":
             // Action for consolidating
-            table.style.color = "green";
+            table.style.background = "green";
             break;
         case "notConsolidate":
             // Action for not consolidate
-            table.style.color = "red"
+            table.style.background = "red"
             break;
         default:
             // Default action
-            table.style.color = "red";
+            table.style.background = "red";
             table.textContent = "Unknown option selected";
     }
+}
+
+function popup() {
+    var popup = document.getElementById("popup");
+    popup.style.visibility = "visible";
 }
