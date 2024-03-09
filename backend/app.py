0fb3c20c27d25001d8558753f60874b7364831bc
Abby M <abbyvmartin51@gmail.com>
2024-03-09 10:32:26 -0500

    add led library

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
 
 @app.route('/')
 def index():
+    GPIO.output(18, GPIO.HIGH)
     return 'Hello world'
 
 if __name__ == '__main__':
