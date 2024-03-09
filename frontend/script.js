95ea52f50b67ed79bc1315952716259ceeb50a40
Abby M <abbyvmartin51@gmail.com>
2024-03-09 12:00:15 -0500

    set up go js

--- frontend/script.js
+++ frontend/script.js
@@ -3,3 +3,32 @@ function popUp() {
     popup.classList.toggle("show");
   }
 
+  const myDiagram =
+  new go.Diagram("roomDiagram",
+    { 
+      "draggingTool.isEnabled": false,
+      "panningTool.isEnabled": false,
+      "dragSelectingTool.isEnabled": false,
+      "maxSelectionCount": 1,
+      "allowHorizontalScroll": false,
+      "allowVerticalScroll": false,
+      "allowZoom": false
+    });
+
+// define a simple Node template
+myDiagram.nodeTemplate =
+  new go.Node("Horizontal",
+    // the entire node will have a light-blue background
+    { background: "#44CCFF" })
+    .add(new go.TextBlock(
+        "",  //default value
+        { margin: 12, stroke: "white", font: "bold 16px sans-serif" })
+        .bind("text", "tableNum"));
+
+myDiagram.model = new go.Model(
+  [ //layout
+
+    { tableNum: "1"},
+    { tableNum: "2"},
+    { tableNum: "3"},
+  ]);
