4471296df273fe939e0c4db2164508aea0b97038
Abby M <abbyvmartin51@gmail.com>
2024-03-09 13:48:28 -0500

    import socket stuff

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,22 @@
 from flask import Flask, request
+from flask_socketio import SocketIO
 import RPi.GPIO as GPIO
 
 app = Flask(__name__)
+socketio = SocketIO(app,debug=True,cors_allowed_origins='*',async_mode='eventlet')
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
 
-# @app.route('/')
-# def index():
-#     return 'Hello world'
+#stored room layouts
+tables = {
+    table1: {
+
+    },
+    table2: {},
+    table3: {},
+    table4:{}
+}
 
 @app.route('/',methods=['GET'])
 def test():
@@ -21,6 +29,7 @@ def test():
         return "light off"
     else:
         return "uh oh"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
